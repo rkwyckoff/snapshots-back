5fd1847473edcbccb0f8557aad570d22624c7be3
Kurt Wyckoff <kurtitservices@gmail.com>
2017-03-16 16:17:46 -0400

    installed sequelize cli and db

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,6 +5,12 @@ const UserController = require("../controllers/user");
 // const CommentController = require("../controllers/comment");
 
 module.exports = (app) => {
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
   app.post('/users', UserController.register);
   app.post('/login', UserController.login);
   // //app.post('/authenticate', middleware.authenticate);
