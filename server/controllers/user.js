1a938a1c8e4886802c57789dec67461fca5365d4
Kurt Wyckoff <kurtitservices@gmail.com>
2017-03-16 15:13:06 -0400

    got route for user registration set up, built user controller, set up middleware file

---
+++ server/controllers/user.js
@@ -0,0 +1,66 @@
+const User = require("../models").User;
+const bcrypt = require("bcryptjs");
+const jwt = require("jwt-simple");
+const appSecrets = require("../config/secrets");
+
+
+module.exports = {
+  register (req, res) {
+    var salt = bcrypt.genSaltSync(10);
+    var hashedPass = bcrypt.hashSync(req.body.password, salt);
+    User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPass,
+      salt: salt,
+
+    })
+      .then(user => res.status(201).send(user))
+      .catch(error => res.status(400).send(error));
+  },
+
+  login (req, res) {
+    User.findOne({
+      where: {
+        email: req.body.email
+      }
+    })
+      .then(user => {
+        if (!user) {
+          return res.status(401).send({ message: "No such email or wrong password." });
+        }
+        console.log("about to hash password ", user);
+        var input = bcrypt.hashSync(req.body.password, user.salt);
+        if (input === user.password) {
+          var token = jwt.encode({ id: user.id, username: user.username }, appSecrets.jwtSecret);
+          console.log("About to encode token", token);
+          var json = {
+            user: user,
+            token: token
+          };
+        return res.status(200).send(json);
+
+        } else {
+          return res.status(401).send({ message: "No such email or wrong password." });
+        }
+      })
+      .catch(error => res.status(400).send(error));
+  },
+
+
+
+
+    listUsers (req, res) {
+        User.findAll({
+      })
+        .then(users => res.status(200).send(users))
+        .catch(error => res.status(400).send(error));
+    },
+
+    findUser (req, res) {
+        User.findById(req.params.id)
+        .then(users => res.status(201).send(users))
+        .catch(error => res.status(400).send(error));
+    }
+
+};
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,24 @@
-// Require your controllers here
+const middleware = require("../middleware/index");
 
+const UserController = require("../controllers/user");
+// const LinkController = require("../controllers/link");
+// const CommentController = require("../controllers/comment");
 
 module.exports = (app) => {
-  // Add your routes here
+  app.post('/users', UserController.register);
+  app.post('/login', UserController.login);
+  // //app.post('/authenticate', middleware.authenticate);
+  // app.post('/link', middleware.authenticate, LinkController.create )
+  // //app.get('/users/:id', middleware.authenticate, UserController.findUser);
+  // app.get('/users', UserController.listUsers);
+  // //app.get('/links', LinkController.listUsers);
+  // app.get('/link', LinkController.listLinks);
+  // app.post('/link/:id/comment', middleware.authenticate, CommentController.create )
+  // app.get('/link/:id/comment', CommentController.listComments )
+  // app.get('/comment', CommentController.listallComments )
+
+
+
+//  app.post('/shelters/:shelterId/animals', AnimalController.create);
+//  app.put('/shelters/:shelterId/animals/:id', AnimalController.update);
 };
