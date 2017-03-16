cab08b84a03f0af243d4c256061f283259527f8d
Kurt Wyckoff <kurtitservices@gmail.com>
2017-03-16 14:18:00 -0400

    did User Migration

---
+++ server/models/user.js
@@ -0,0 +1,17 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var User = sequelize.define('User', {
+    username: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    salt: DataTypes.STRING,
+    profile_url: DataTypes.STRING
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return User;
+};
