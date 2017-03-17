1d4aa5b6c8cea3e18448043100b32f644d69a310
Kurt Wyckoff <kurtitservices@gmail.com>
2017-03-17 17:08:27 -0400

    added function for getting photo and user in one request

--- server/controllers/photo.js
+++ server/controllers/photo.js
@@ -34,7 +34,7 @@ module.exports = {
       User.findById(photo.user_id)
       .then(user => {
         photo.user = user;
-        res.status(200).send(photo);
+        res.status(200).send(photo, user);
       })
   })
 }
