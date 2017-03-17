8a269fc0b5de837a84d8de6a33948d842343013f
Kurt Wyckoff <kurtitservices@gmail.com>
2017-03-17 18:06:55 -0400

    made changes to photo function

--- server/models/photo.js
+++ server/models/photo.js
@@ -10,8 +10,9 @@ module.exports = function(sequelize, DataTypes) {
   }, {
     classMethods: {
       associate: function(models) {
-        // associations can be defined here
-      }
+          User.hasMany(models.Photo, {
+            foreignKey: 'userId'
+          });
     }
   });
   Photo.belongsTo(User);
