e7bf6bbf67d66d40df494d4e42ecbf2e0ca34c04
Kurt Wyckoff <kurtitservices@gmail.com>
2017-03-17 08:40:31 -0400

    cleaning.

--- server/controllers/photo.js
+++ server/controllers/photo.js
@@ -21,8 +21,10 @@ module.exports = {
   },
 
   listPhoto (req, res) {
-    Photo.findById(req.params.id {
-
+    Photo.findById({
+        where:  {
+          id: req.params.id
+        }
       })
     .then(photo => res.status(201).send(photo))
     .catch(error => res.status(400).send(error));
